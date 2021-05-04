30beb90075f4e6554152f68885461f6c14054c9b
Longoon12000 <longoon12000@gmail.com>
2021-05-04 15:45:09 +0200

    Initial upload of Visual Studio solution

---
+++ ETS2 Button Box/ETS2 Button Box Arduino/ETS2 Button Box Arduino.ino
@@ -0,0 +1,9 @@
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
