f3538c5e1662eddeb6777c33c0d679e47948ae1d
Longoon12000 <longoon12000@gmail.com>
2021-05-16 21:29:48 +0200

    Adds basic serial interaction for LEDs

--- ETS2 Button Box/ETS2 Button Box Arduino/ETS2 Button Box Arduino.ino
+++ ETS2 Button Box/ETS2 Button Box Arduino/ETS2 Button Box Arduino.ino
@@ -1,9 +1,287 @@
-// the setup function runs once when you press reset or power the board
+#include <Adafruit_MCP23017.h>
+
+// ### WARNING! ###
+// This code is made for use with the development board
+// which is very different than the production board!
+
+#pragma region External hardware
+// U1 on development board
+Adafruit_MCP23017 mcp1;
+// U2 on development board
+Adafruit_MCP23017 mcp2;
+// U3 on development board
+Adafruit_MCP23017 mcp3;
+#pragma endregion
+
+#pragma region IO Definitions
+/// <summary>
+/// Store required information for accessing buttons or LEDs using MCP23017
+/// </summary>
+struct IO_DEFINITION {
+	Adafruit_MCP23017* mcp;
+	uint8_t pin;
+};
+
+// IO definitions for accessing LEDs using MCP23017
+const IO_DEFINITION leds[] = {
+	{ &mcp1, 0 },	// LED_CC
+	{ &mcp1, 1 },	// LED_GR
+	{ &mcp1, 2 },	// LED_DIF
+	{ &mcp1, 3 },	// LED_IND_L
+	{ &mcp1, 4 },	// LED_IND_R
+	{ &mcp1, 5 },	// LED_IND_H
+	{ &mcp1, 6 },	// LED_BR
+	{ &mcp1, 7 },	// LED_RET
+	{ &mcp1, 8 },	// LED_ENB
+	{ &mcp1, 9 },	// LED_EB
+	{ &mcp1, 10 },	// LED_TOW
+	{ &mcp1, 11 },	// LED_HRN
+	{ &mcp1, 12 },	// LED_SCR
+	{ &mcp1, 13 },	// LED_FLS
+	{ &mcp1, 14 },	// LED_PWR
+	{ &mcp1, 15 },	// LED_ENG
+	{ &mcp2, 0 },	// LED_EF
+	{ &mcp2, 1 },	// LED_F0
+	{ &mcp2, 2 },	// LED_F1
+	{ &mcp2, 3 },	// LED_F2
+	{ &mcp2, 4 },	// LED_F3
+	{ &mcp2, 5 },	// LED_F4
+	{ &mcp2, 6 },	// LED_F5
+	{ &mcp2, 7 },	// LED_F6
+	{ &mcp2, 8 },	// LED_F7
+	{ &mcp2, 9 },	// LED_F8
+	{ &mcp2, 10 },	// LED_F9
+	{ &mcp2, 11 }	// LED_F10
+};
+
+// IO definitions for accessing buttons using MCP23017
+// Note: uses readButton(IO_DEFINITION* def)
+const IO_DEFINITION buttons[] = {
+	{ &mcp2, 12 },	// BTN_CCO
+	{ &mcp2, 13 },	// BTN_CCR
+	{ &mcp2, 14 },	// BTN_CCS1
+	{ &mcp2, 15 },	// BTN_CCS2
+	{ &mcp3, 0 },	// BTN_GR
+	{ &mcp3, 1 },	// BTN_DIF
+	{ &mcp3, 2 },	// BTN_VW1
+	{ &mcp3, 3 },	// BTN_VW2
+	{ &mcp3, 4 },	// BTN_VW3
+	{ &mcp3, 5 },	// BTN_VW4
+	{ &mcp3, 6 },	// BTN_INDH
+	{ &mcp3, 7 },	// BTN_BC
+	{ &mcp3, 8 },	// BTN_LI1
+	{ &mcp3, 9 },	// BTN_LI2
+	{ &mcp3, 10 },	// BTN_LI3
+	{ &mcp3, 11 },	// BTN_LI4
+	{ &mcp3, 12 },	// BTN_WI1
+	{ &mcp3, 13 },	// BTN_WI2
+	{ &mcp3, 14 },	// BTN_WI3
+	{ &mcp3, 15 }	// BTN_WI4
+};
+
+// Note: These indices must be equal to the corresponding values of the LED enum of the host application
+#define LED_CC 0
+#define LED_GR 1
+#define LED_DIF 2
+#define LED_IND_L 3
+#define LED_IND_R 4
+#define LED_IND_H 5
+#define LED_BR 6
+#define LED_RET 7
+#define LED_ENB 8
+#define LED_EB 9
+#define LED_TOW 10
+#define LED_HRN 11
+#define LED_SCR 12
+#define LED_FLS 13
+#define LED_PWR 14
+#define LED_ENG 15
+#define LED_EF 16
+#define LED_F0 17
+#define LED_F1 18
+#define LED_F2 19
+#define LED_F3 20
+#define LED_F4 21
+#define LED_F5 22
+#define LED_F6 23
+#define LED_F7 24
+#define LED_F8 25
+#define LED_F9 26
+#define LED_F10 27
+
+// Note: These indices must be equal to the corresponding values of the Button enum of the host application
+#define BTN_CCO 0
+#define BTN_CCR 1
+#define BTN_CCS1 2
+#define BTN_CCS2 3
+#define BTN_GR 4
+#define BTN_DIF 5
+#define BTN_VW1 6
+#define BTN_VW2 7
+#define BTN_VW3 8
+#define BTN_VW4 9
+#define BTN_INDH 10
+#define BTN_BC 11
+#define BTN_LI1 12
+#define BTN_LI2 13
+#define BTN_LI3 14
+#define BTN_LI4 15
+#define BTN_WI1 16
+#define BTN_WI2 17
+#define BTN_WI3 18
+#define BTN_WI4 19
+
+// Note: remaining buttons are not accessed through MCP23017 but dierctly through pins on the Arduino
+// Note: uses readButton(uint8_t pin)
+const uint8_t BTN_TOW = 2;
+const uint8_t BTN_HRN = 3;
+const uint8_t BTN_SCR = 4;
+const uint8_t BTN_FLS = 5;
+const uint8_t BTN_RET = 6;
+const uint8_t BTN_RET1 = 7;
+const uint8_t BTN_RET2 = 8;
+const uint8_t BTN_ENB = 9;
+const uint8_t BTN_ENB1 = 10;
+const uint8_t BTN_ENB2 = 11;
+const uint8_t BTN_EB = 12;
+const uint8_t BTN_PWR = A0;
+const uint8_t BTN_ENG = A1;
+#pragma endregion
+
+#pragma region IO Functions
+/// <summary>
+/// Enable an LED
+/// </summary>
+/// <param name="def">LED IO definition reference</param>
+void enableLED(const IO_DEFINITION* def) {
+	(*(*def).mcp).digitalWrite((*def).pin, HIGH);
+}
+
+/// <summary>
+/// Disable an LED
+/// </summary>
+/// <param name="def">LED IO definition reference</param>
+void disableLED(const IO_DEFINITION* def) {
+	(*(*def).mcp).digitalWrite((*def).pin, LOW);
+}
+
+/// <summary>
+/// Read a button based on an IO definition
+/// </summary>
+/// <param name="def">Button IO definition reference</param>
+/// <returns>true when button is pressed, false when released</returns>
+bool readButton(const IO_DEFINITION* def) {
+	return !(*(*def).mcp).digitalRead((*def).pin);
+}
+
+/// <summary>
+/// Read a button based on an internal Arduino port
+/// </summary>
+/// <param name="pin">Arduino pin number</param>
+/// <returns>true when button is pressed, false when released</returns>
+bool readButton(const uint8_t pin) {
+	return !digitalRead(pin);
+}
+#pragma endregion
+
+#pragma region Serial
+void handleLedFromSerial() {
+	// Read serial data until the delimiter character is reached
+	String data = Serial.readStringUntil('\x00');
+	if (data.length() == 0)
+		return;
+
+	// data contains a string of 0 and 1 in order of the LED enum as per host application
+
+	// Check that received data is of equal length as the LED IO definitions
+	if (sizeof(leds) / sizeof(IO_DEFINITION) != data.length())
+	{
+		// Either less or more LEDs have been sent over serial
+		// To indicate this error to the user we will disable all LEDs...
+		for (size_t i = 0; i < sizeof(leds) / sizeof(IO_DEFINITION); i++)
+			disableLED(&leds[i]);
+
+		// ...and only enable PWR and EF to show a fault and IND_L to show an LED data mismatch failure
+		enableLED(&leds[LED_PWR]);
+		enableLED(&leds[LED_EF]);
+		enableLED(&leds[LED_IND_L]);
+
+		// Stop further processing
+		return;
+	}
+
+	// Loop through all LEDs in the leds IO definition
+	for (size_t i = 0; i < sizeof(leds) / sizeof(IO_DEFINITION); i++)
+	{
+		// Enable or disable each corresponding LED
+		if (data.charAt(i) == '1')
+			enableLED(&leds[i]);
+		else
+			disableLED(&leds[i]);
+	}
+}
+
+void sendButtonData() {
+	// TODO: implement
+}
+#pragma endregion
+
+#pragma region Arduino workflow
+/// <summary>
+/// Arduino power-on setup
+/// </summary>
 void setup() {
+	// Setup serial connection for interacting with the host application
+	Serial.begin(9600);
 
+	// Initialise MCP23017
+	mcp1.begin((uint8_t)0);
+	mcp2.begin((uint8_t)1);
+	mcp3.begin((uint8_t)2);
+
+	// Initialise U1 for LEDs and turn them off
+	for (size_t i = 0; i < 16; i++) {
+		mcp1.pinMode(i, OUTPUT);
+		mcp1.digitalWrite(i, LOW);
+	}
+
+	// Initialise U2 for LEDs and turn them off
+	for (size_t i = 0; i < 12; i++) {
+		mcp2.pinMode(i, OUTPUT);
+		mcp2.digitalWrite(i, LOW);
+	}
+
+	// Initialise U2 for buttons
+	for (size_t i = 12; i < 16; i++)
+	{
+		mcp2.pinMode(i, INPUT);
+		mcp2.pullUp(i, HIGH);
+	}
+
+	// Initialise U3 for buttons
+	for (size_t i = 0; i < 16; i++) {
+		mcp3.pinMode(i, INPUT);
+		mcp3.pullUp(i, HIGH);
+	}
+
+	// Initialise internal pins for buttons
+	for (size_t i = 2; i < 13; i++)
+		pinMode(i, INPUT_PULLUP);
+	pinMode(A0, INPUT_PULLUP);
+	pinMode(A1, INPUT_PULLUP);
+
+	// Initially PWR and IND_H LED is enabled to show button box is powered and ready for connection
+	enableLED(&leds[LED_PWR]);
+	enableLED(&leds[LED_IND_H]);
 }
 
-// the loop function runs over and over again until power down or reset
+/// <summary>
+/// Arduino main loop
+/// </summary>
 void loop() {
-  
+	// TODO: handle button inputs
+	// TODO: make sure this doesn't take more than 100ms in total
+
+	handleLedFromSerial();
 }
+#pragma endregion
--- ETS2 Button Box/ETS2 Button Box Arduino/__vm/.ETS2 Button Box Arduino.vsarduino.h
+++ ETS2 Button Box/ETS2 Button Box Arduino/__vm/.ETS2 Button Box Arduino.vsarduino.h
@@ -6,114 +6,88 @@
 			All non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 			Note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: Generic ESP8266 Module, Platform=esp8266, Package=esp8266
+	Hardware: ATmega328P (Old Bootloader) (Arduino Nano), Platform=avr, Package=arduino
 */
 
 #if defined(_VMICRO_INTELLISENSE)
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-#define __ESP8266_esp8266__
-#define __ESP8266_ESP8266__
-#define __ets__
-#define ICACHE_FLASH
-#define NONOSDK22x_190703 1
-#define F_CPU 80000000L
-#define LWIP_OPEN_SRC
-#define TCP_MSS 536
-#define LWIP_FEATURES 1
-#define LWIP_IPV6 0
+#define __AVR_atmega328p__
+#define __AVR_ATmega328P__
+#define __AVR_ATmega328p__
+#define _VMDEBUG 1
+#define F_CPU 16000000L
 #define ARDUINO 108013
-#define ARDUINO_ESP8266_GENERIC
-#define ARDUINO_ARCH_ESP8266
-#define LED_BUILTIN 2
-#define FLASHMODE_DOUT
-#define ESP8266
+#define ARDUINO_AVR_NANO
+#define ARDUINO_ARCH_AVR
 #define __cplusplus 201103L
+#define _Pragma(x)
+#define __AVR__
+#define __inline__
+#define __asm__(...)
+#define __extension__
+#define __inline__
+#define __volatile__
+// Redefine __cplusplus to correct version: https://www.visualmicro.com/forums/YaBB.pl?num=1592217268
 #undef __cplusplus
 #define __cplusplus 201103L
-#define __STDC__
-#define __ARM__
-#define __arm__
-#define __inline__
-#define __asm__(x)
-#define __asm__
-#define __extension__
-#define __ATTR_PURE__
-#define __ATTR_CONST__
-#define __volatile__
+
+//#define GCC_VERSION 40902
+//https://www.visualmicro.com/forums/YaBB.pl?num=1569762585/5#5
+#define __GNUC__             5
+#define __GNUC_MINOR__       4
+#define __GNUC_PATCHLEVEL__  0
+#define GCC_VERSION ((__GNUC__*10000)+(__GNUC_MINOR__*100)+__GNUC_PATCHLEVEL__)) 
 
 
-#define __ASM
-#define __INLINE
-#define __attribute__(noinline)
-
-//#define _STD_BEGIN
-//#define EMIT
-#define WARNING
-#define _Lockit
-#define __CLR_OR_THIS_CALL
-#define C4005
-#define _NEW
-
-//typedef int uint8_t;
-//#define __ARMCC_VERSION 400678
-//#define PROGMEM
-//#define string_literal
-//
-//#define prog_void
-//#define PGM_VOID_P int
-//
-
-typedef int _read;
-typedef int _seek;
-typedef int _write;
-typedef int _close;
-typedef int __cleanup;
-
-//#define inline 
-
-#define __builtin_clz
-#define __builtin_clzl
-#define __builtin_clzll
-#define __builtin_labs
-#define __builtin_va_list
-typedef int __gnuc_va_list;
-
-#define __ATOMIC_ACQ_REL
-
-#define __CHAR_BIT__
-#define _EXFUN()
-
-typedef unsigned char byte;
-extern "C" void __cxa_pure_virtual() {;}
+#define volatile(va_arg) 
+#define _CONST
+#define __builtin_va_start
+#define __builtin_va_end
+#define __attribute__(...)
+#define NOINLINE __attribute__((noinline))
+#define prog_void
+#define PGM_VOID_P int
 
 
-typedef long __INTPTR_TYPE__ ;
-typedef long __UINTPTR_TYPE__ ;
-typedef long __SIZE_TYPE__ 	;
-typedef long __PTRDIFF_TYPE__;
+#ifndef __builtin_constant_p
+	#define __builtin_constant_p __attribute__((__const__))
+#endif
+#ifndef __builtin_strlen
+	#define __builtin_strlen  __attribute__((__const__))
+#endif
 
 
-#include "new"
-#include "Esp.h"
+#define NEW_H
+typedef void *__builtin_va_list;
+//extern "C" void __cxa_pure_virtual() {;}
+
+typedef int div_t;
+typedef int ldiv_t;
+
+
+typedef void *__builtin_va_list;
+//extern "C" void __cxa_pure_virtual() {;}
+
 
 
 #include "arduino.h"
-#include <common.h> 
 #include <pins_arduino.h> 
-
-#include "..\generic\Common.h"
-#include "..\generic\pins_arduino.h"
-
-#undef F
-#define F(string_literal) ((const PROGMEM char *)(string_literal))
+//#undef F
+//#define F(string_literal) ((const PROGMEM char *)(string_literal))
 #undef PSTR
 #define PSTR(string_literal) ((const PROGMEM char *)(string_literal))
-//current vc++ does not understand this syntax so use older arduino example for intellisense
-//todo:move to the new clang/gcc project types.
-#define interrupts() sei()
-#define noInterrupts() cli()
+
+//typedef unsigned char uint8_t;
+//typedef unsigned int uint8_t;
+
+#define pgm_read_byte_near(address_short) uint8_t()
+#define pgm_read_byte(address_short) uint8_t() 
+#define pgm_read_word(address_short) uint16_t() 
+#define pgm_read_dword(address_short) uint32_t()
+#define pgm_read_float(address_short) float()
+#define pgm_read_ptr(address_short)   short()
 
 #include "ETS2 Button Box Arduino.ino"
 #endif
