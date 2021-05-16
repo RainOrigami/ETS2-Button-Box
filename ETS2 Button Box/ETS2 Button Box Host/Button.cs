94f9d30ac746250401ad1f8073657e4d903956f3
Longoon12000 <longoon12000@gmail.com>
2021-05-16 21:22:04 +0200

    Adds LED and button enumerations

---
+++ ETS2 Button Box/ETS2 Button Box Host/Button.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETS2_Button_Box_Host
+{
+    /// <summary>
+    /// Available buttons on the button box
+    /// </summary>
+    public enum Button
+    {
+        /// <summary>
+        /// Cruise control enable
+        /// </summary>
+        CCO,
+        /// <summary>
+        /// Cruise control resume
+        /// </summary>
+        CCR,
+        /// <summary>
+        /// Cruise control rotary encoder pin 1
+        /// </summary>
+        CCS1,
+        /// <summary>
+        /// Cruise control rotary encoder pin 2
+        /// </summary>
+        CCS2,
+        /// <summary>
+        /// Gear range high/low
+        /// </summary>
+        GR,
+        /// <summary>
+        /// Differential lock engage/disengage
+        /// </summary>
+        DIF,
+        /// <summary>
+        /// View selector selection 1
+        /// </summary>
+        VW1,
+        /// <summary>
+        /// View selector selection 2
+        /// </summary>
+        VW2,
+        /// <summary>
+        /// View selector selection 3
+        /// </summary>
+        VW3,
+        /// <summary>
+        /// View selector selection 4
+        /// </summary>
+        VW4,
+        /// <summary>
+        /// Hazard indicator toggle
+        /// </summary>
+        INDH,
+        /// <summary>
+        /// Beacon on/off
+        /// </summary>
+        BC,
+        /// <summary>
+        /// Lights selector selection 1
+        /// </summary>
+        LI1,
+        /// <summary>
+        /// Lights selector selection 2
+        /// </summary>
+        LI2,
+        /// <summary>
+        /// Lights selector selection 3
+        /// </summary>
+        LI3,
+        /// <summary>
+        /// Lights selector selection 4
+        /// </summary>
+        LI4,
+        /// <summary>
+        /// Wiper selector selection 1
+        /// </summary>
+        WI1,
+        /// <summary>
+        /// Wiper selector selection 2
+        /// </summary>
+        WI2,
+        /// <summary>
+        /// Wiper selector selection 3
+        /// </summary>
+        WI3,
+        /// <summary>
+        /// Wiper selector selection 4
+        /// </summary>
+        WI4,
+        /// <summary>
+        /// Trailer connection toggle
+        /// </summary>
+        TOW,
+        /// <summary>
+        /// Horns momentary (Normal horns)
+        /// </summary>
+        HRN,
+        /// <summary>
+        /// Screamer momentary (Loud horns)
+        /// </summary>
+        SCR,
+        /// <summary>
+        /// Flasher momentary (momentary high beam)
+        /// </summary>
+        FLS,
+        /// <summary>
+        /// Retarder on/off
+        /// </summary>
+        RET,
+        /// <summary>
+        /// Retarder strength rotary encoder pin 1
+        /// </summary>
+        RET1,
+        /// <summary>
+        /// Retarder strength rotary encoder pin 2
+        /// </summary>
+        RET2,
+        /// <summary>
+        /// Engine brake on/off
+        /// </summary>
+        ENB,
+        /// <summary>
+        /// Engine brake strength rotary encoder pin 1
+        /// </summary>
+        ENB1,
+        /// <summary>
+        /// Engine brake strength rotary encoder pin 2
+        /// </summary>
+        ENB2,
+        /// <summary>
+        /// E-Brake on/off
+        /// </summary>
+        EB,
+        /// <summary>
+        /// Systems power on/off
+        /// </summary>
+        PWR,
+        /// <summary>
+        /// Engine power enable
+        /// </summary>
+        ENG
+    }
+}
---
+++ ETS2 Button Box/ETS2 Button Box Host/LED.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETS2_Button_Box_Host
+{
+    /// <summary>
+    /// Available LEDs on the button box
+    /// </summary>
+    public enum LED
+    {
+        /// <summary>
+        /// Flasher (momentary high beam)
+        /// </summary>
+        FLS,
+        /// <summary>
+        /// Screamer (Loud horns)
+        /// </summary>
+        SCR,
+        /// <summary>
+        /// Horns (Normal horns)
+        /// </summary>
+        HRN,
+        /// <summary>
+        /// Trailer connection status
+        /// </summary>
+        TOW,
+        /// <summary>
+        /// E-Brake status
+        /// </summary>
+        EB,
+        /// <summary>
+        /// Engine brake status
+        /// </summary>
+        ENB,
+        /// <summary>
+        /// Retarder status
+        /// </summary>
+        RET,
+        // TODO: remember!
+        /// <summary>
+        /// I forgot.
+        /// </summary>
+        BR,
+        /// <summary>
+        /// Indicator hazards
+        /// </summary>
+        INDH,
+        /// <summary>
+        /// Indicator right
+        /// </summary>
+        INDR,
+        /// <summary>
+        /// Indicator left
+        /// </summary>
+        INDL,
+        /// <summary>
+        /// Differential lock status
+        /// </summary>
+        DIF,
+        /// <summary>
+        /// Gear range
+        /// </summary>
+        GR,
+        /// <summary>
+        /// Cruise control status
+        /// </summary>
+        CC,
+        /// <summary>
+        /// Systems power status
+        /// </summary>
+        PWR,
+        /// <summary>
+        /// Engine power status
+        /// </summary>
+        ENG,
+        /// <summary>
+        /// Engine fault
+        /// </summary>
+        EF,
+        /// <summary>
+        /// Fuel 0%
+        /// </summary>
+        F0,
+        /// <summary>
+        /// Fuel 10%
+        /// </summary>
+        F1,
+        /// <summary>
+        /// Fuel 20%
+        /// </summary>
+        F2,
+        /// <summary>
+        /// Fuel 30%
+        /// </summary>
+        F3,
+        /// <summary>
+        /// Fuel 40%
+        /// </summary>
+        F4,
+        /// <summary>
+        /// Fuel 50%
+        /// </summary>
+        F5,
+        /// <summary>
+        /// Fuel 60%
+        /// </summary>
+        F6,
+        /// <summary>
+        /// Fuel 70%
+        /// </summary>
+        F7,
+        /// <summary>
+        /// Fuel 80%
+        /// </summary>
+        F8,
+        /// <summary>
+        /// Fuel 90%
+        /// </summary>
+        F9,
+        /// <summary>
+        /// Fuel 100%
+        /// </summary>
+        F10
+    }
+}
