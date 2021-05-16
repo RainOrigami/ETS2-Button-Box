11846c9eed949fba2620b8e7cd1caf2f96f5c84f
Longoon12000 <longoon12000@gmail.com>
2021-05-16 21:28:53 +0200

    Adds basic serial and SDK interaction for LEDs

--- ETS2 Button Box/ETS2 Button Box Host/ETS2ButtonBoxHost.Designer.cs
+++ ETS2 Button Box/ETS2 Button Box Host/ETS2ButtonBoxHost.Designer.cs
@@ -1,3 +1,4 @@
+﻿
 namespace ETS2_Button_Box_Host
 {
     partial class ETS2ButtonBoxHost
@@ -29,8 +30,81 @@ namespace ETS2_Button_Box_Host
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.cbCOMPort = new System.Windows.Forms.ComboBox();
+            this.lblCOMPort = new System.Windows.Forms.Label();
+            this.btnReload = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.toolTips = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
+            // 
+            // cbCOMPort
+            // 
+            this.cbCOMPort.FormattingEnabled = true;
+            this.cbCOMPort.Location = new System.Drawing.Point(74, 12);
+            this.cbCOMPort.Name = "cbCOMPort";
+            this.cbCOMPort.Size = new System.Drawing.Size(180, 21);
+            this.cbCOMPort.TabIndex = 0;
+            this.toolTips.SetToolTip(this.cbCOMPort, "List of available COM ports.\r\nSelect the COM port that is the button box.\r\nIf you" +
+        " are not sure which one to choose try the one with the highest number.");
+            // 
+            // lblCOMPort
+            // 
+            this.lblCOMPort.AutoSize = true;
+            this.lblCOMPort.Location = new System.Drawing.Point(12, 15);
+            this.lblCOMPort.Name = "lblCOMPort";
+            this.lblCOMPort.Size = new System.Drawing.Size(56, 13);
+            this.lblCOMPort.TabIndex = 1;
+            this.lblCOMPort.Text = "COM Port:";
+            // 
+            // btnReload
+            // 
+            this.btnReload.Image = global::ETS2_Button_Box_Host.Properties.Resources.Refresh_16x;
+            this.btnReload.Location = new System.Drawing.Point(260, 10);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(23, 23);
+            this.btnReload.TabIndex = 4;
+            this.toolTips.SetToolTip(this.btnReload, "Refresh the list of COM ports");
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Image = global::ETS2_Button_Box_Host.Properties.Resources.Connect_16x;
+            this.btnConnect.Location = new System.Drawing.Point(289, 10);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(23, 23);
+            this.btnConnect.TabIndex = 2;
+            this.toolTips.SetToolTip(this.btnConnect, "Connect to the selected COM port");
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // ETS2ButtonBoxHost
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 46);
+            this.Controls.Add(this.btnReload);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.lblCOMPort);
+            this.Controls.Add(this.cbCOMPort);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "ETS2ButtonBoxHost";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "ETS2 Button Box Host";
+            this.Load += new System.EventHandler(this.ETS2ButtonBoxHost_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbCOMPort;
+        private System.Windows.Forms.Label lblCOMPort;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnReload;
+        private System.Windows.Forms.ToolTip toolTips;
+    }
+}
+
--- ETS2 Button Box/ETS2 Button Box Host/ETS2ButtonBoxHost.cs
+++ ETS2 Button Box/ETS2 Button Box Host/ETS2ButtonBoxHost.cs
@@ -14,5 +14,402 @@ namespace ETS2_Button_Box_Host
 {
     public partial class ETS2ButtonBoxHost : Form
     {
+        /// <summary>
+        /// Delimiter character for sending and receiving serial data
+        /// </summary>
+        private const char delimiter = '\x00';
+
+        /// <summary>
+        /// Time between toggling blinking LEDs in milliseconds
+        /// </summary>
+        private const double ledBlinkTimeout = 1000;
+
+        /// <summary>
+        /// Time between sending LED status to button box in milliseconds
+        /// </summary>
+        private const double ledSendInterval = 100;
+
+        /// <summary>
+        /// Defines the threshold under which fuel percentage value which LEDs are turned on
+        /// </summary>
+        private readonly IReadOnlyDictionary<int, LED> fuelLedThreshold = new Dictionary<int, LED>()
+        {
+            { 5, LED.F1 },
+            { 10, LED.F2 },
+            { 20, LED.F3 },
+            { 30, LED.F4 },
+            { 40, LED.F5 },
+            { 50, LED.F6 },
+            { 60, LED.F7 },
+            { 70, LED.F8 },
+            { 80, LED.F9 },
+            { 90, LED.F10 }
+        };
+
+        /// <summary>
+        /// Run mode of the application
+        /// </summary>
+        private enum RunMode
+        {
+            /// <summary>
+            /// Connection to the button box has been established
+            /// </summary>
+            Connected = 1,
+            /// <summary>
+            /// ETS2 SDK is connected to a game instance
+            /// </summary>
+            Active = 2,
+            /// <summary>
+            /// Whether or not the scheduler is currently active
+            /// </summary>
+            Scheduler = 4
+        }
+
+        /// <summary>
+        /// Map of all current LED states
+        /// </summary>
+        private Dictionary<LED, bool> ledStates;
+
+        /// <summary>
+        /// ETS2 SDK client
+        /// </summary>
+        private SCSSdkTelemetry telemetry;
+
+        /// <summary>
+        /// Serial connection to the button box
+        /// </summary>
+        private SerialPort buttonBoxPort;
+
+        /// <summary>
+        /// Current run mode of the application
+        /// </summary>
+        private RunMode runMode;
+
+        /// <summary>
+        /// In (RunMode.Connected | RunMode.Inactive) mode stores the last LED toggle time for blinking LEDs
+        /// </summary>
+        private DateTime inactiveLedLastBlinkTime;
+
+        /// <summary>
+        /// Stores the time of the last time the LED status was sent to the button box
+        /// </summary>
+        private DateTime lastLEDStatusSendTime;
+
+        /// <summary>
+        /// Stores the time of the last time the fuel LED has toggled in low fuel state
+        /// </summary>
+        private DateTime lastFuelWarningTime;
+
+        /// <summary>
+        /// Whether or not there is a fuel warning (fuel < fuelWarn%)
+        /// </summary>
+        private bool isFuelWarning;
+
+        public ETS2ButtonBoxHost()
+        {
+            // WinForms initialisation
+            InitializeComponent();
+
+            // Initialise run mode to not connected, inactive and no scheduler running
+            this.runMode = 0;
+
+            // Initialise last LED toggle time to the current time
+            this.inactiveLedLastBlinkTime = DateTime.Now;
+
+            // Initialise last LED status time to the current time
+            this.lastLEDStatusSendTime = DateTime.Now;
+
+            // Initialise last fuel warning LED time to current time
+            this.lastFuelWarningTime = DateTime.Now;
+
+            // Initialise the LED states map
+            this.ledStates = new Dictionary<LED, bool>();
+
+            // Fill LED states map with all available LEDs and turn them OFF
+            foreach (LED led in Enum.GetValues(typeof(LED)))
+                this.ledStates.Add(led, false);
+
+            // Initialise serial connection to button box
+            this.buttonBoxPort = new SerialPort();
+            this.buttonBoxPort.BaudRate = 9600;
+            this.buttonBoxPort.Parity = Parity.None;
+            this.buttonBoxPort.DataBits = 8;
+            this.buttonBoxPort.StopBits = StopBits.One;
+            this.buttonBoxPort.DataReceived += this.buttonBoxPort_DataReceived;
+
+            // Initialise ETS2 SDK client
+            this.telemetry = new SCSSdkTelemetry();
+            this.telemetry.Data += this.telemetry_Data;
+        }
+
+        /// <summary>
+        /// Disables all LEDs.
+        /// This has no effect until writeLEDStates is called.
+        /// </summary>
+        private void resetLEDs()
+        {
+            foreach (LED led in Enum.GetValues(typeof(LED)))
+                disableLED(led);
+        }
+
+        /// <summary>
+        /// Disable specific LED.
+        /// This has no effect until writeLEDStates is called.
+        /// </summary>
+        /// <param name="led">LED to disable</param>
+        private void disableLED(LED led)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(() => disableLED(led)));
+                return;
+            }
+
+            lock (this.ledStates)
+                this.ledStates[led] = false;
+        }
+
+        /// <summary>
+        /// Enables specific LED.
+        /// This has no effect until writeLEDStates is called.
+        /// </summary>
+        /// <param name="led">LED to enable</param>
+        private void enableLED(LED led)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(() => enableLED(led)));
+                return;
+            }
+
+            lock (this.ledStates)
+                this.ledStates[led] = true;
+        }
+
+        /// <summary>
+        /// Toggle (invert) the state of a specific LED.
+        /// This has no effect until writeLEDStates is called.
+        /// </summary>
+        /// <param name="led">LED to toggle</param>
+        private void toggleLED(LED led)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(() => toggleLED(led)));
+                return;
+            }
+
+            lock (this.ledStates)
+                this.ledStates[led] = !this.ledStates[led];
+        }
+
+        /// <summary>
+        /// ETS2 SDK data has been received
+        /// </summary>
+        private void telemetry_Data(SCSSdkClient.Object.SCSTelemetry data, bool newTimestamp)
+        {
+            // Check for SDK connection to a game instance
+            if (!data.SdkActive)
+            {
+                // Disable Active and set Inactive run mode
+                this.runMode &= ~RunMode.Active;
+            }
+            else if ((this.runMode & RunMode.Active) == 0)
+            {
+                this.runMode |= RunMode.Active;
+
+                // Enable the LED scheduler
+                if ((this.runMode & RunMode.Scheduler) == 0)
+                    Task.Run(ledScheduler);
+            }
+
+            // Do nothing if SDK not connected or button box not connected
+            if ((this.runMode & RunMode.Active) == 0 || (this.runMode & RunMode.Connected) == 0)
+                return;
+
+            // Calculate percentage of fuel
+            int fuelPercentage = (int)Math.Round(data.TruckValues.CurrentValues.DashboardValues.FuelValue.Amount / data.TruckValues.ConstantsValues.CapacityValues.Fuel * 100f);
+            
+            // Get fuel warning indicator
+            this.isFuelWarning = data.TruckValues.CurrentValues.DashboardValues.WarningValues.FuelW;
+            
+            // When no fuel warning occurs set F0 LED fixed on
+            if (!this.isFuelWarning)
+                this.enableLED(LED.F0);
+
+            // Set individual F1-F10 LEDs based on the fuelLedThreshold
+            foreach (int threshold in fuelLedThreshold.Keys)
+            {
+                if (fuelPercentage > threshold)
+                    this.enableLED(fuelLedThreshold[threshold]);
+                else
+                    this.disableLED(fuelLedThreshold[threshold]);
+            }
+        }
+
+        /// <summary>
+        /// Serial data from button box received.
+        /// Handles all buttons, switches, rotary encoders and selectors.
+        /// </summary>
+        private void buttonBoxPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Post initialisation routine gets UI elements ready for user interaction
+        /// </summary>
+        private void ETS2ButtonBoxHost_Load(object sender, EventArgs e)
+        {
+            this.reloadCOMPorts();
+        }
+
+        /// <summary>
+        /// Fill available COM ports into COM port combobox
+        /// </summary>
+        private void reloadCOMPorts()
+        {
+            this.cbCOMPort.Items.Clear();
+            this.cbCOMPort.Items.AddRange(SerialPort.GetPortNames());
+        }
+
+        /// <summary>
+        /// Connect to COM port button click handler
+        /// </summary>
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            // Get and validate the selected COM port
+            string comPort = this.cbCOMPort.SelectedItem as string;
+
+            if (string.IsNullOrEmpty(comPort))
+            {
+                MessageBox.Show("Please select a COM port from the list.", "No COM port selected", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!SerialPort.GetPortNames().Contains(comPort))
+            {
+                MessageBox.Show("The selected COM port is not available anymore. Please check the connection and try again.", "COM port not available", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.reloadCOMPorts();
+                return;
+            }
+
+            // Assign COM port and open connection to button box
+            this.buttonBoxPort.PortName = comPort;
+            this.buttonBoxPort.Open();
+
+            if (this.buttonBoxPort.IsOpen)
+                this.runMode |= RunMode.Connected;
+
+            // Enable the LED scheduler
+            if ((this.runMode & RunMode.Scheduler) == 0)
+                Task.Run(ledScheduler);
+        }
+
+        /// <summary>
+        /// LED scheduler handles updates to the LEDs that happen outside of the ETS2 SDK data handler (telemetry_Data)
+        /// </summary>
+        private async void ledScheduler()
+        {
+            this.runMode |= RunMode.Scheduler;
+
+            while (true)
+            {
+                // Exit LED scheduler if not connected to button box anymore
+                if (!this.buttonBoxPort.IsOpen || (this.runMode & RunMode.Connected) == 0)
+                {
+                    this.runMode &= ~RunMode.Scheduler;
+                    return;
+                }
+
+                // Handle blinking all LEDs in (RunMode.Connected | RunMode.Inactive) mode
+                if ((this.runMode & RunMode.Connected) != 0 && (this.runMode & RunMode.Active) == 0)
+                {
+                    // Wait for LED blink timeout
+                    if ((DateTime.Now - this.inactiveLedLastBlinkTime).TotalMilliseconds >= ledBlinkTimeout)
+                    {
+                        // Toggle all LEDs
+                        foreach (LED led in this.ledStates.Keys.ToArray())
+                            this.toggleLED(led);
+
+                        // Set last inactive LED toggle time to now
+                        this.inactiveLedLastBlinkTime = DateTime.Now;
+                    }
+                }
+                else if ((this.runMode & RunMode.Connected) != 0 && (this.runMode & RunMode.Active) != 0)
+                {
+                    if (this.isFuelWarning)
+                    {
+                        this.disableLED(LED.F1);
+                        this.disableLED(LED.F2);
+                        this.disableLED(LED.F3);
+                        this.disableLED(LED.F4);
+                        this.disableLED(LED.F5);
+                        this.disableLED(LED.F6);
+                        this.disableLED(LED.F7);
+                        this.disableLED(LED.F8);
+                        this.disableLED(LED.F9);
+                        this.disableLED(LED.F10);
+
+                        if ((DateTime.Now - this.lastFuelWarningTime).TotalMilliseconds >= ledBlinkTimeout)
+                        {
+                            this.toggleLED(LED.F0);
+                            this.lastFuelWarningTime = DateTime.Now;
+                        }
+                    }
+                }
+
+                // Send the current LED states to the button box
+                await this.writeLEDStates();
+            }
+        }
+
+        /// <summary>
+        /// Writes LED states to button box.
+        /// Only does so when connection is established and program state is Connected.
+        /// Respects and waits for ledSendInterval between sends.
+        /// </summary>
+        private async Task writeLEDStates()
+        {
+            // Check if connection to button box is open and if run mode is in valid state for sending LED states
+            if (!this.buttonBoxPort.IsOpen || (this.runMode & RunMode.Connected) == 0)
+                return;
+
+            // Wait, if necessary, for the next ledSendInterval
+            TimeSpan timeSinceLastLEDStatusSend = DateTime.Now - this.lastLEDStatusSendTime;
+            if (timeSinceLastLEDStatusSend.TotalMilliseconds < ledSendInterval)
+                await Task.Delay((int)(ledSendInterval - timeSinceLastLEDStatusSend.TotalMilliseconds));
+
+            // Invoke process on main thread
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(async () => await writeLEDStates()));
+                return;
+            }
+
+            // Send LED state to button box
+#if DEBUG
+            string ledStateString = this.getLEDStateString();
+            Console.WriteLine($"Sending {ledStateString}");
+            this.buttonBoxPort.Write($"{ledStateString}{delimiter}");
+#else
+            this.buttonBoxPort.Write($"{this.getLEDStateString()}{delimiter}");
+#endif
+            this.lastLEDStatusSendTime = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Build the LED state string that is being sent to the button box
+        /// </summary>
+        /// <returns>String of 0 or 1 for each available LED</returns>
+        private string getLEDStateString() => String.Join("", Enum.GetValues(typeof(LED)).OfType<LED>().OrderBy(led => (int)led).Select(led => this.ledStates[led] ? "1" : "0"));
+
+        /// <summary>
+        /// Reload COM ports button click handler
+        /// </summary>
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            this.reloadCOMPorts();
+        }
     }
 }
