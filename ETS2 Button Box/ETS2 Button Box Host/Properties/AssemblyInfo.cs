1a1b6f217467b56baa41bd8a858b1a272eaf803c
Longoon12000 <longoon12000@gmail.com>
2021-05-16 21:19:45 +0200

    Updates assembly information

--- ETS2 Button Box/ETS2 Button Box Host/Properties/AssemblyInfo.cs
+++ ETS2 Button Box/ETS2 Button Box Host/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ETS2 Button Box Host")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Handles serial IO from the ETS2 Button Box and the ETS2 SDK")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("CodeFreak.net")]
 [assembly: AssemblyProduct("ETS2 Button Box Host")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
